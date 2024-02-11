0d045fdebe5af684d807baebe63472d825b0e8d4
Sneha Deshmukh <112338973+snehaDeshmukh576@users.noreply.github.com>
2024-02-11 22:37:29 +0530

    Add files via upload

---
+++ 2.js
@@ -0,0 +1,11 @@
+console.log("lets learn conditional statements")
+let age=1;
+let grace=2;
+age+=grace
+console.log(age)
+console.log(age+grace)
+console.log(age-grace)
+console.log(age*grace)
+console.log(age/grace)
+console.log(age**grace)
+console.log(age%grace)

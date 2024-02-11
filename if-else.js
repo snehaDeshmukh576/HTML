7d3fbd8d786b2c413aaa19b194e1276562db0179
Sneha Deshmukh <112338973+snehaDeshmukh576@users.noreply.github.com>
2024-02-11 22:46:57 +0530

    Add files via upload

---
+++ if-else.js
@@ -0,0 +1,13 @@
+let age=5;
+if(age==18){
+    console.log("You can drive")
+}
+else if(age==0){
+    console.log("are you kidding")
+}
+else if(age<18){
+    console.log("you  cannot drive")
+}
+else{
+    console.log("you can  drive")
+}

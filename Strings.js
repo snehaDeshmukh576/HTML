6575b086ff5f32e3abb479da5504b39c12920943
Sneha Deshmukh <112338973+snehaDeshmukh576@users.noreply.github.com>
2024-02-13 00:45:13 +0530

    Add files via upload

---
+++ Strings.js
@@ -0,0 +1,27 @@
+let a="Sneha";
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[2]);
+console.log(a[3]);
+console.log(a[4]);
+// console.log(a[5]);
+
+console.log(a.length)
+
+let real_name="Aishwarya"
+let friend="Shreya"
+console.log("her name is "+real_name+" and her friends name is "+friend)
+console.log(`Her name is ${real_name} and her friends name is ${friend}`)
+
+
+let b="Shivamsh"
+console.log(b.toUpperCase())
+console.log(b.toLowerCase())
+console.log(b.length)
+console.log(b.slice(1,5))
+console.log(b.slice(1))
+
+console.log(b.replace("Sh","77"))
+console.log(b.concat(a,"Aishwarya","Rahul","Priya"))
+
+console.log(b)
